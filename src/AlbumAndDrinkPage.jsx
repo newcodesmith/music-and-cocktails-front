aa4d79c6803f399c1d0e58814a768ef9e36bee42
Michael Smith <michael@Michaels-MacBook-Pro-6.local>
2018-05-15 11:46:50 -0600

    add GET for album info, create UserDrinkInfoCard.jsx

--- src/AlbumAndDrinkPage.jsx
+++ src/AlbumAndDrinkPage.jsx
@@ -3,13 +3,28 @@ import './App.css';
 
 import queryString from "query-string"
 import SpotifyPlayer from 'react-spotify-player';
+import UserDrinkInfoCard from './UserDrinkInfoCard.jsx';
 
 class AlbumAndDrinkPage extends Component {
-
-
     state = {
         albumId: this.props.location.state,
-        accessToken: queryString.parse(window.location.search).access_token
+        accessToken: queryString.parse(window.location.search).access_token,
+        albumData: []
+    };
+
+    getAlbum() {
+        const albumId = this.state.albumId;
+        const albumUrl = `http://localhost:3000/albums/${albumId}`;
+        let albumDataGrab = response => {
+            this.setState({ albumData: response });
+        };
+        return fetch(albumUrl)
+            .then(response => response.json())
+            .then(albumDataGrab)
+    };
+
+    componentDidMount() {
+        this.getAlbum();
     }
 
     render() {
@@ -18,6 +33,9 @@ class AlbumAndDrinkPage extends Component {
             <div>
                 <h1>This is the Album and Drink Page</h1>
                 <h2>{albumId}</h2>
+                <UserDrinkInfoCard 
+                drinkInfo= {this.state.albumData}
+                />
             </div>
         )
     }
---
+++ src/UserDrinkInfoCard.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class UserDrinkInfoCard extends Component {
+
+    render() {
+        const drinkInfo = this.props.drinkInfo;
+        // console.log(drinkInfo);
+        
+        return (
+            <div className="admin-drink-info-card">
+                <h1>Current Paired Drink Info</h1>
+                <h3>{drinkInfo.drink_title}</h3>
+                <div className="admin-drink-info">
+                    <div className="">
+                        <img src={drinkInfo.drink_pic_url} alt={drinkInfo.drink_title} height="150" />
+                    </div>
+                    <div className="">
+                        <ul>
+                            <li>{drinkInfo.drink_description}</li>
+                            <li>
+                                {drinkInfo.ingredients}
+                            </li>
+                            <li>{drinkInfo.direction}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default UserDrinkInfoCard;
