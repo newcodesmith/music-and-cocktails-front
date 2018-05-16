21cd32aa622f87f0c11fe710bc06b8729af5dc9a
Michael Smith <michael@Michaels-MacBook-Pro-6.local>
2018-05-15 22:06:14 -0600

    add styles

--- src/UserAlbumCard.jsx
+++ src/UserAlbumCard.jsx
@@ -5,18 +5,18 @@ import SpotifyPlayer from 'react-spotify-player';
 
 class UserAlbumCard extends Component {
     state = {
-        ...this.props,       
+        ...this.props,
         spotifyAlbumData: []
     };
 
     getSpotifyData() {
-        const albumInfo = this.state.albumData;
-        const accessToken = this.state.accessToken;
-        const userData = this.state.userData;
+        const albumInfo = this.props.albumData;
+        const accessToken = this.props.accessToken;
+        const userData = this.props.userData;
 
-        const spotifyUrl = `https://api.spotify.com/v1/albums/${albumInfo.spotify_album_id}?market=${userData.country}`;
+        const spotifyUrl = `https://api.spotify.com/v1/albums/42oBdomfxF0DbKKMEqrnQW?market=US}`;
         console.log(spotifyUrl);
-        
+
 
         let spotifyDataGrab = response => {
             this.setState({ spotifyAlbumData: response });
@@ -39,22 +39,44 @@ class UserAlbumCard extends Component {
     }
 
     render() {
-        
-        const albumInfo = this.state.albumData;
-        
+        const albumInfo = this.props.albumData;
+
+
+        // if (albumInfo.length > 1) {
+        // } else {
+        //     this.setState(...this.props)
+        //     this.getSpotifyData();
+        //     console.log("did this run?");
+
+        // }
+
+
         const size = {
             width: '50%',
-            height: 300,
+            height: 400,
         };
         const view = 'list'; // or 'coverart'
         const theme = 'black'; // or 'white'
 
         return (
             <div>
-                <div>
-                    <h1>This months {albumInfo.genre} album is "{albumInfo.album_title}" by {albumInfo.artist}</h1>
-                    <h2>About the album</h2>
-                    <p>{albumInfo.album_info}</p>
+                <h1>{albumInfo.genre}</h1>
+                <div className="album-cover-info ">
+                    <div className="flip-container" ontouchstart="this.classList.toggle('hover');">
+                        <div className="flipper">
+                            <div className={`front front-${albumInfo.album_id}`}>
+                                <h1>{albumInfo.artist}:</h1>
+                                <h1>{albumInfo.album_title}</h1>
+                            </div>
+                            <div className={`back back-${albumInfo.album_id}`}>
+                                <img src="https://is1-ssl.mzstatic.com/image/thumb/Music7/v4/23/ab/8a/23ab8ab2-a0d4-d824-7b0d-5532df236f12/UMG_cvrart_00602547305732_01_RGB72_1500x1500_13UABIM03512.jpg/1200x630bb.jpg" alt={albumInfo.album_title} />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="about-album">
+                        <h2>About the album</h2>
+                        <p>{albumInfo.album_info}</p>
+                    </div>
                 </div>
                 <div>
                     <SpotifyPlayer
